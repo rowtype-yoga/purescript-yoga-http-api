d790ae472d4bc739a4162936c144a2ed7d34d2bd
Mark <mark.eibes@gmail.com>
2026-02-08 00:50:06 +0100

    refactor: Use public write API and point-free style in validate

--- test/Test/OpenAPIValidation.purs
+++ test/Test/OpenAPIValidation.purs
@@ -1,10 +1,11 @@
 module Test.OpenAPIValidation (validate) where
 
+import Prelude
 import Foreign (Foreign)
 import Yoga.HTTP.API.Route.OpenAPI (OpenAPISpec)
-import Yoga.JSON (writeImpl)
+import Yoga.JSON (write)
 
 foreign import validateImpl :: Foreign -> { errors :: Array { message :: String } }
 
 validate :: OpenAPISpec -> { errors :: Array { message :: String } }
-validate spec = validateImpl (writeImpl spec)
+validate = validateImpl <<< write
